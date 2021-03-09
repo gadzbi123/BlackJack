ee82e95062a77bf364d832279fcea9b81b2d27fc
gadzbi123 dom <crg1234@gmail.com>
2021-03-09 09:48:11 +0100

    xd

--- lab1/lab1/lab1.cpp
+++ lab1/lab1/lab1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class Firma
 {
 public:
-    int do_uregulowania=0;
+    int do_uregulowania = 0;
 
     void uregulowane()
     {
@@ -12,7 +12,7 @@ public:
     virtual ~Firma() = default;
 };
 
-class Dostawca: public Firma
+class Dostawca : public Firma
 {
 public:
     virtual void dostawa()//nie wykonuje sie - zastosowalem to aby wiedziec co ma robic metoda
@@ -21,7 +21,7 @@ public:
     }
 };
 
-class Nabywca: public Firma
+class Nabywca : public Firma
 {
 public:
     virtual void sprzedaz()//nie wykonuje sie - zastosowalem to aby wiedziec co ma robic metoda
@@ -68,11 +68,11 @@ int main()
 {
     Monitory monitoryZRajhu;
     Komputery komputer;
-    Hurt hurtGliwice,hurtKato;
-    Detal *detal=new Detal[3];
+    Hurt hurtGliwice, hurtKato;
+    Detal* detal = new Detal[3];
 
     Firma* firmy[] = { &komputer,&monitoryZRajhu,&hurtGliwice,&hurtKato,&detal[0],&detal[1],&detal[2] };
- 
+
     int wartosc = 0;
 
     for (auto i : firmy)
@@ -91,10 +91,10 @@ int main()
             cout << "Nazwa dostawcy i kwota: ";
         }
 
-        cout << typeid(*i).name() << ", " << i->do_uregulowania<<" zl\n";//nie podanie wskaznika w typeid() skutkuje wypisaniem wskaznika na firme- klasy rodzica
+        cout << typeid(*i).name() << ", " << i->do_uregulowania << " zl\n";//nie podanie wskaznika w typeid() skutkuje wypisaniem wskaznika na firme- klasy rodzica
         wartosc += i->do_uregulowania;
     }
-    cout << "Saldo: " << wartosc<<" zl\n";
-    delete []detal;
+    cout << "Saldo: " << wartosc << " zl\n";
+    delete[]detal;
 }
 
