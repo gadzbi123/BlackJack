e9a46fedf72fbeeaefc2fd93c940543573b4a982
gadzbi123 dom <crg1234@gmail.com>
2021-04-04 13:18:33 +0200

    xd

--- lab6/lab6/main.cpp
+++ lab6/lab6/main.cpp
@@ -2,50 +2,58 @@
 #include<vector>
 
 using namespace std;
-class A
+
+//Zad1
+class Centrum_Handlowe
 {
 public:
-	int a;
-	A() :a(5) { cout << "Konstruktor bezArg" << endl; }
-	A(int x) : a(x) { cout << "Konstruktor jednoArg" << endl; }
-	~A() { cout << "Destruktor" << endl; }
-	void Increment() { cout << "Increment" << endl; this->a++; }
+	int sklepy;
+	Centrum_Handlowe() :sklepy(0) { cout << "Konstruktor bezArg" << endl; }
+	Centrum_Handlowe(int count) :sklepy(count) { cout << "Konstruktor jednoArg" << endl; }
+	~Centrum_Handlowe() { cout << "Destruktor" << endl; }
+	void Increment() { this->sklepy++; cout << "Increment" << endl; }
 };
 
-void dzien_1()
+//Zad 2,3,4.
+void wypisz_wsk(const unique_ptr<Centrum_Handlowe>&x,const string name)
 {
-	unique_ptr<A>wsk_kl1(new A(10));
-	unique_ptr<A>wsk_kl2(new A);
-	unique_ptr<A>wsk_kl3 = make_unique <A> (5);
-	//unique_ptr<A>wsk_kl3 = wsk_kl2; NIE MOZNA PRZYPISAC BO MUSI BYC UNIQUE
-	//unique_ptr<A>wsk_kl3 = move(wsk_kl2); USUNIE wsk_kl2 
+	if (x)
+		cout << name << ": adres=" << x << " sklepy=" << x->sklepy << endl;
+	else
+		cout << name << ": adres=nullptr"<<endl;
+}
+
+void zadania()
+{
+	unique_ptr<Centrum_Handlowe>wsk_kl1(new Centrum_Handlowe(10));
+	unique_ptr<Centrum_Handlowe>wsk_kl2= make_unique <Centrum_Handlowe>(5);
+	unique_ptr<Centrum_Handlowe>wsk_kl3(new Centrum_Handlowe);
+	//unique_ptr<A>wsk_kl3 = wsk_kl2; //UNIQUE - wyj¹tkowy
+	//unique_ptr<A>wsk_kl3 = move(wsk_kl2); //USUNIE wsk_kl2 ale przypisze do wsk_kl3
 	wsk_kl1->Increment();
 
-
-
-	cout << "wskkl1=" << wsk_kl1->a << endl;
-	cout << "wskkl2=" << wsk_kl2->a << endl;
+	wypisz_wsk(wsk_kl1, "wskkl1");
+	wypisz_wsk(wsk_kl2, "wskkl2");
+	
 	//swap
 	wsk_kl1.swap(wsk_kl2);
-	cout << "wskkl1=" << wsk_kl1->a << endl;
-	cout << "wskkl2=" << wsk_kl2->a << endl;
+	wypisz_wsk(wsk_kl1, "wskkl1");
+	wypisz_wsk(wsk_kl2, "wskkl2");
 
 	//reset
-	cout << "Adres przed reset:" << wsk_kl1.get() << "\n";
+	cout << "Adres przed reset=>";
+	wypisz_wsk(wsk_kl1, "wskkl1");
 	wsk_kl1.reset();
-	cout << "Adres po reset:" << wsk_kl1.get() << '\n';
+	cout << "Adres po reset=>";
+	wypisz_wsk(wsk_kl1, "wskkl1");
 
 	//release
-	cout << "Adres przed release:" << wsk_kl2.get() << "\n";
+	cout << "Adres przed release=>";
+	wypisz_wsk(wsk_kl2, "wskkl2");
 	auto ptr = wsk_kl2.release();
-	cout << "Adres po release:" << wsk_kl2.get() << '\n';//wyciek
+	cout << "Adres po release=>";
+	wypisz_wsk(wsk_kl2, "wskkl2");//wyciek
 	delete ptr;
-
-	//bool s_ptr
-	if (wsk_kl1)
-		cout << "wskkl1 istnieje" << endl;
-	else
-		cout << "wskkl1 nie istnieje" << endl;
 
 	shared_ptr<int>wsk_int1 = make_unique<int>(5);
 	shared_ptr<int>wsk_int2 = wsk_int1;
@@ -59,38 +67,7 @@ void dzien_1()
 
 }
 
-weak_ptr<int> slaby;
-
-void observe()
-{
-	cout <<"liczba uzyc slabego="<< slaby.use_count() << endl;
-	if (auto dzielony = slaby.lock())//przypisz weaka do shared aby go uzyæ potem, nie mozna dzielony = slaby bo to nie jest przypisanie do tego samego typu
-		cout << *dzielony << endl;//nie mozna wypisac weaka dlatego uzywamy shared
-	else
-		cout << "slaby przestal istniec"<<endl;
-
-
-}
-
-void dzien_2()
-{
-	{
-		auto shared = make_shared<int>(50);
-		slaby = shared;
-		observe();
-	}
-	observe();
-}
-
-class Centrum_Handlowe
-{
-public:
-	int sklepy;
-	Centrum_Handlowe():sklepy(0) { cout << "Konstruktor bezArg" << endl; }
-	Centrum_Handlowe(int count) :sklepy(count) { cout << "Konstruktor jednoArg" << endl; }
-	~Centrum_Handlowe() { cout << "Destruktor" << endl; }
-};
-
+//ZAD 5
 void wypisz_vec_ptr(vector<shared_ptr<int>> x)
 {
 	for (const auto p : x) {
@@ -110,32 +87,44 @@ void wypisz_adresy(vector<shared_ptr<int>> x)
 	cout << endl;
 }
 
-
-void zad5()
+void zadanie_5()
 {
 	vector<shared_ptr<int>>wsk_int;
 	for (int i = 0; i < 10; i++)
 		wsk_int.push_back(make_shared<int>(rand() % 15));
 
 	wypisz_vec_ptr(wsk_int);
+	wypisz_adresy(wsk_int);
 	wsk_int[2] = wsk_int[3];
 	wsk_int[4] = wsk_int[3];
 	weak_ptr<int>weak = wsk_int[3];
+
 	cout << "weak.use_count:" << weak.use_count() << " Adres weak:" << weak.lock() << endl;
+
 	wsk_int[3].reset();
 	wsk_int[2].reset();
-	cout << "weak.use_count:" << weak.use_count() << " Adres weak:" << weak.lock() << endl;//poniewaz weak.use_count !=0 to istnieje adres weak.lock
+	cout << "weak.use_count:" << weak.use_count() << " Adres weak:" << weak.lock() << endl;//poniewaz weak.use_count() !=0 to istnieje adres weak.lock()
+
 	wsk_int[4].reset();
-	cout << "weak.use_count:" << weak.use_count() << " Adres weak:" << weak.lock() << endl;
+	cout << "weak.use_count:" << weak.use_count() << " Adres weak:" << weak.lock() << endl;//weak.use_count()==0 czyli weak.lock()=0
+
 	wypisz_vec_ptr(wsk_int);
 	wypisz_adresy(wsk_int);
 }
 
 int main()
 {
-	//dzien_1();
-	//dzien_2();
-	
+	zadania();
+	zadanie_5();
+	shared_ptr<int> p1(new int(10),
+		[](int* p)
+		{
+			cout << "Zwolnienie pamieci o adresie" << p<<
+				"wskazujaca na wartoœæ" << *p << endl;
+			delete p;
+		});
+
+	return 0;
 }
 
 //reset usuwa obiekt i zeruje wskaznik
