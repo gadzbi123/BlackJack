c5335e5a1899552810214002c36ea5af7e6de567
gadzbi123 dom <crg1234@gmail.com>
2021-04-08 09:35:58 +0200

    xd

--- NaukaSFML/NaukaSFML/main.cpp
+++ NaukaSFML/NaukaSFML/main.cpp
@@ -3,6 +3,19 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Beginner",sf::Style::Close | sf::Style::Titlebar| sf::Style::Resize); 
+    sf::RectangleShape player(sf::Vector2f(100.0f,150.0f));
+    player.setPosition(400, 300);
+    window.setKeyRepeatEnabled(false);
+
+    sf::Texture playerTexture;
+    playerTexture.loadFromFile("texture/tux_from_linux.png");
+    player.setTexture(&playerTexture);
+
+    sf::Vector2u textureSize = playerTexture.getSize();
+    textureSize.x /= 3;
+    textureSize.y /= 9;
+
+    player.setTextureRect(sf::IntRect(textureSize.x * 2, textureSize.y*8, textureSize.x, textureSize.y));
 
     while (window.isOpen())
     {
@@ -14,32 +27,32 @@ int main()
             case sf::Event::Closed:
                 window.close();
                 break;
-            case sf::Event::Resized:
-                std::cout << "New width and height: " << evnt.size.width << " " << evnt.size.height << std::endl;
-                break;
             case sf::Event::MouseWheelScrolled:
                 std::cout << "Wheel movement: " << evnt.mouseWheelScroll.delta << std::endl;
                 std::cout << "mouse x: " << evnt.mouseWheelScroll.x << std::endl;
                 std::cout << "mouse y: " << evnt.mouseWheelScroll.y << std::endl;
                 break;
-            case sf::Event::MouseButtonPressed:
-                if (evnt.mouseButton.button == sf::Mouse::Right)
-                    std::cout << "Right button pressed" << std::endl;
-                if (evnt.mouseButton.button == sf::Mouse::Left)
-                    std::cout << "Left button pressed" << std::endl;
-                if (evnt.mouseButton.button == sf::Mouse::Middle)
-                    std::cout << "Middle button pressed" << std::endl;
-                break;
-            case sf::Event::LostFocus:
-                std::cout << "Paused" << std::endl;
-                break;
-            case sf::Event::GainedFocus:
-                std::cout << "Resumed" << std::endl;
+            case sf::Event::TextEntered:
+                if (evnt.text.unicode < 128)
+                    printf("%c", evnt.text.unicode);
                 break;
 
             }
         }
-        
+        window.setFramerateLimit(850);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+            player.move(-0.1f, 0.0f);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+            player.move(0.1f, 0.0f);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+            player.move(0.0f, -0.1f);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+            player.move(0.0f, 0.1f);
+        window.clear();
+        window.draw(player);
+        window.display();
+
+
     }
 
     return 0;
---
+++ lab7/lab7/lab7.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <condition_variable>
+#include <fstream>
+
+static std::mutex mtx;
+std::condition_variable cv;
+
+void funkcjaKopiujWektor(const std::vector<int> & a, std::vector<int> & b) {
+	for (auto i : a)
+	{
+		std::unique_lock<std::mutex>blokada(mtx);
+		std::cout << std::this_thread::get_id() << std::endl;
+		b.push_back(i);
+	}
+}
+
+class CopyVector
+{
+public:
+	void operator()(const std::vector<int>& a, std::vector<int>& b) {
+		for (auto i : a)
+		{
+			std::lock_guard<std::mutex>blokada(mtx);
+			std::cout << std::this_thread::get_id() << std::endl;
+			b.push_back(i);
+		}
+	}
+};
+
+class ZapiszDoPliku
+{
+private:
+ 	std::string sciezkaDoPliku;
+	std::vector<std::string>slowa;
+
+public:
+	ZapiszDoPliku(const std::string& sciezkaDoPliku) : sciezkaDoPliku(sciezkaDoPliku) {}
+	void Wypelnij()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			std::string temp;
+			std::cin >> temp;
+			slowa.push_back(std::move(temp));
+			cv.notify_all();
+		}
+	}
+	void Zapisz()
+	{
+		std::ofstream plik(sciezkaDoPliku);
+		for (int i = 0; i < 5; i++)
+		{
+			std::unique_lock < std::mutex> lck(mtx);
+			cv.wait(lck, [=] {return i + 1 <= slowa.size(); });
+			plik << slowa[i] << '\n';
+		}
+		plik.close();
+	}
+	
+};
+
+int main()
+{
+	//Zad1
+	std::cout << std::thread::hardware_concurrency() << std::endl;
+
+	//Zad 2
+	std::vector<int> wek1 = { 3,13,44,22,66,88,25,11 };
+	std::vector<int> wek2 = { 113,183,244,522,766,888,925,111 };
+	std::vector<int> wek3;
+
+	
+
+	CopyVector objekt;
+	std::thread t1(funkcjaKopiujWektor, std::cref(wek1), ref(wek3));
+	std::thread t2(std::ref(objekt), cref(wek2), ref(wek3));
+	
+	t1.join();
+	t2.join();
+	
+	for (auto i : wek3)
+	{
+		std::cout << (i) << " ";
+	}
+	std::cout << std::endl;
+
+	//Zad3
+	ZapiszDoPliku plik("zadanie.txt");
+	std::thread t3(&ZapiszDoPliku::Wypelnij, &plik);
+	std::thread t4(&ZapiszDoPliku::Zapisz, &plik);
+
+	std::cout << "Podaj 5 slow" << std::endl;
+	t3.join();
+	t4.join();
+}
